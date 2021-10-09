4f9ad34d0dce9a8c2c2dc7012fc927a88026efe2
NkemdiAnyiam <NkemdiAnyiam@gmail.com>
2021-10-09 00:19:19 -0500

    Prevent collapse/expand animation on page load

--- scripts/common-questions.js
+++ scripts/common-questions.js
@@ -1,5 +1,6 @@
 const addCollapseFunctionality = () => {
   const questions = document.querySelector(".questions");
+
   questions.addEventListener('click', (e) => {
     const clicked = e.target.closest('.expand-btn');
   
@@ -7,8 +8,18 @@ const addCollapseFunctionality = () => {
   
     const qaBlock = clicked.closest('.questions__qa-block');
     const question = qaBlock.querySelector('.questions__answer');
-    question.classList.toggle('collapsed');
-    question.classList.toggle('expanded');
+
+    // if not expanded, add 'expanded' and remove 'collapsed'
+    if(!question.classList.contains('expanded')) {
+      question.classList.add('expanded');
+      question.classList.remove('collapsed');
+    }
+    // otherwise, do the reverse
+    else {
+      question.classList.add('collapsed');
+      question.classList.remove('expanded');
+    }
+    // toggle button class state
     clicked.classList.toggle('expand');
   });
 };
