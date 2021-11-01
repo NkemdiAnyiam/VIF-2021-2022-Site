2661bd67fefc6da0929307054af1acf5dbb8ae17
NkemdiAnyiam <NkemdiAnyiam@gmail.com>
2021-10-31 20:27:34 -0500

    Separater footer into separate file

--- scripts/global.js
+++ scripts/global.js
@@ -23,6 +23,39 @@ const injectBanner = () => {
 };
 injectBanner();
 
+const injectFooter = () => {
+  const footer = document.querySelector('.footer');
+  footer.innerHTML = '';
+  footer.insertAdjacentHTML('beforeend', `
+    <ul class="footer__links">
+      <li class="footer__item">
+        <a href="https://twitter.com/tamuviz?t=GzuSJdHsO5U7NpkGwtHXBA&s=09" target="_blank" class="footer__link">
+          <ion-icon name="logo-twitter"></ion-icon>
+        </a>
+      </li>
+
+      <li class="footer__item">
+        <a href="https://youtube.com/c/TexasAMDepartmentofVisualization" target="_blank" class="footer__link">
+          <ion-icon name="logo-youtube"></ion-icon>
+        </a>
+      </li>
+      
+      <li class="footer__item">
+        <a href="https://instagram.com/tamu_vizindustryfair?utm_medium=copy_link" target="_blank" class="footer__link">
+          <ion-icon name="logo-instagram"></ion-icon>
+        </a>
+      </li>
+
+      <li class="footer__item">
+        <a href="https://www.linkedin.com/company/texas-a-m-visualization-industry-fair" target="_blank" class="footer__link">
+          <ion-icon name="logo-linkedin"></ion-icon>
+        </a>
+      </li>
+    </ul>
+  `);
+};
+injectFooter();
+
 // const injectNav = () => {
 //   const navigation = document.querySelector('.nav');
 //   const pathSubstring = window.location.pathname.substring(window.location.pathname.lastIndexOf('/') + 1)
