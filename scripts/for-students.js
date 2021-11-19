ed39170438364738e4b50fb2ac571f6cbc80c2bc
NkemdiAnyiam <NkemdiAnyiam@gmail.com>
2021-11-18 22:11:06 -0600

    Add current list of companies attending fair

--- scripts/for-students.js
+++ scripts/for-students.js
@@ -1,32 +1,49 @@
 const companies = [
-  "Aggieland Outfitters",
-  "Texas Film Commission",
-  "Texas Moving Picture Alliance (TXMPA)",
-  "Electronic Arts",
-  "NOKIA",
-  "N3D Studios Incorporated ",
-  "Powerhouse Animation",
-  "SunSpear Games",
-  "Pumps & Pipes",
-  "Houston Methodist Institute for Technology, Innovation and Education",
-  "Gearbox Software",
-  "Aquifer",
-  "Digital Seat Media",
-  "Enduring Games",
-  "C.C. Creations",
-  "Booz Allen Hamilton, Inc.",
-  "Cloud Imperium Games",
+  {companyName: "Aggieland Outfitters"},
+  {companyName: "Texas Film Commission"},
+  {companyName: "Texas Moving Picture Alliance (TXMPA)"},
+  {companyName: "Electronic Arts"},
+  {companyName: "NOKIA"},
+  {companyName: "N3D Studios Incorporated"},
+  {companyName: "Powerhouse Animation"},
+  {companyName: "SunSpear Games"},
+  {companyName: "Pumps & Pipes"},
+  {companyName: "Houston Methodist Institute for Technology, Innovation and Education"},
+  {companyName: "Gearbox Software"},
+  {companyName: "Aquifer"},
+  {companyName: "Digital Seat Media"},
+  {companyName: "Enduring Games"},
+  {companyName: "C.C. Creations"},
+  {companyName: "Booz Allen Hamilton, Inc."},
+  {companyName: "Cloud Imperium Games"},
 ];
 
-companies.sort();
+const companies2 = [
+  {companyName: "Jacobs", focus: "Animation"},
+  {companyName: "IMG Studio", focus: "Animation"},
+  {companyName: "K2Share", focus: "Graphic Design"},
+  {companyName: "Blur Studio Inc", focus: "Animation"},
+  {companyName: "BonusXP", focus: "Game Development"},
+];
 
-const fillPastCompaniesList = () => {
-  const companiesList = document.querySelector('.companies-list');
+const companiesNameSortComparator = (company1, company2) => {
+  return (company1.companyName <= company2.companyName) ? -1 : 1;
+}
+
+companies.sort(companiesNameSortComparator);
+companies2.sort(companiesNameSortComparator);
+
+const fillCompaniesList = (listClass, list) => {
+  const companiesList = document.querySelector(`.${listClass}`);
   companiesList.innerHTML = '';
-  companies.forEach((companyName) => {
+  list.forEach(({companyName, focus}) => {
     companiesList.insertAdjacentHTML('beforeend', `
-      <li class="companies-list__item">${companyName}</li>
+      <li class="companies-list__item">
+        <span class="companies-list__name">${companyName}</span>
+        ${focus ? `<span class="companies-list__focus">${focus}</span>` : ''}
+      </li>
     `);
   });
 };
-fillPastCompaniesList();
+fillCompaniesList('companies-list--current', companies2);
+fillCompaniesList('companies-list--past', companies);
