dfa60fba303b5ab9564d0239e71971f2fc5f9eee
NkemdiAnyiam <NkemdiAnyiam@gmail.com>
2022-02-16 20:33:56 -0600

    Add links to companies' websites on current companies list

--- scripts/for-students.js
+++ scripts/for-students.js
@@ -1,22 +1,22 @@
 const companiesPresent = [
-  {companyName: "Jacobs", focus: "Animation"},
-  {companyName: "IMG Studio", focus: "Animation"},
-  {companyName: "K2Share", focus: "Graphic Design"},
-  {companyName: "Blur Studio Inc", focus: "Animation"},
-  {companyName: "BonusXP", focus: "Game Development"},
-  {companyName: "Credera", focus: "Web UI/UX"},
-  {companyName: "Cloud Imperium Games", focus: "Game Development"},
-  {companyName: "900lbs", focus: "Animation | Graphic Design | Game Development | VFX"},
-  {companyName: "Click Here Labs - TRG", focus: "Animation"},
-  {companyName: "Vytal Studios", focus: "Metaverse Platform Creation"},
-  {companyName: "The Third Floor, Inc.", focus: "Film | TV | VR | Games"},
-  {companyName: "Mighty Coconut", focus: "Game Development"},
-  {companyName: "DreamWorks Animation", focus: "Animation"},
-  {companyName: "VIBE", focus: "Architecture Visualization"},
-  {companyName: "Electronic Arts", focus: "Game Development"},
-  {companyName: "TRG", focus: "Interactive Design"},
-  {companyName: "Cockram Scenario", focus: "Themed Entertainment"},
-  {companyName: "Virtuix Inc.", focus: "Game Development"},
+  {companyName: "Jacobs", focus: "Animation", url: "http://www.jacobs.com/"},
+  {companyName: "IMG Studio", focus: "Animation", url: "https://theimgstudio.com/"},
+  {companyName: "K2Share", focus: "Graphic Design", url: "https://www.k2share.com/"},
+  {companyName: "Blur Studio Inc", focus: "Animation", url: "http://blur.com/"},
+  {companyName: "BonusXP", focus: "Game Development", url: "https://bonusxp.com/"},
+  {companyName: "Credera", focus: "Web UI/UX", url: "https://credera.com/"},
+  {companyName: "Cloud Imperium Games", focus: "Game Development", url: "https://cloudimperiumgames.com/"},
+  {companyName: "900lbs", focus: "Animation | Graphic Design | Game Development | VFX", url: "https://900lbs.com/"},
+  {companyName: "Click Here Labs - TRG", focus: "Animation", url: "https://clickherelabs.com/"},
+  {companyName: "Vytal Studios", focus: "Metaverse Platform Creation", url: "https://vytalstudios.com/"},
+  {companyName: "The Third Floor, Inc.", focus: "Film | TV | VR | Games", url: "https://thethirdfloorinc.com/"},
+  {companyName: "Mighty Coconut", focus: "Game Development", url: "https://www.mightycoconut.com/"},
+  {companyName: "DreamWorks Animation", focus: "Animation", url: "https://www.dreamworks.com/"},
+  {companyName: "VIBE", focus: "Architecture Visualization", url: "https://viberenders.com/"},
+  {companyName: "Electronic Arts", focus: "Game Development", url: "https://www.ea.com/"},
+  {companyName: "TRG", focus: "Interactive Design", url: "https://richards.com/"},
+  {companyName: "Cockram Scenario", focus: "Themed Entertainment", url: "https://www.teaconnect.org/Members/Member-Directory/index.cfm?membercode=8255"},
+  {companyName: "Virtuix Inc.", focus: "Game Development", url: "https://www.virtuix.com/"},
 ];
 
 const companiesPast = [
@@ -49,10 +49,10 @@ companiesPresent.sort(companiesNameSortComparator);
 const fillCompaniesList = (listClass, list) => {
   const companiesList = document.querySelector(`.${listClass}`);
   companiesList.innerHTML = '';
-  list.forEach(({companyName, focus}) => {
+  list.forEach(({companyName, focus, url}) => {
     companiesList.insertAdjacentHTML('beforeend', `
       <li class="companies-list__item">
-        <span class="companies-list__name">${companyName}</span>
+        ${url ? `<a href="${url}" class="link" target="_blank">` : ''}<span class="companies-list__name">${companyName}</span>${url ? `</a>` : ''}
         ${focus ? `<span class="companies-list__focus">${focus}</span>` : ''}
       </li>
     `);
